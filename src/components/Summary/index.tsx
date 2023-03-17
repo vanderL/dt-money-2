af5978adfc781598813f4d08973d9a10afa429b1
Vander Lima <vnd.vander@gmail.com>
2023-03-16 21:12:15 -0300

    feat: Summary transactions

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,6 +6,26 @@ import { SummaryCard, SummaryContainer } from "./styles";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+
+  )
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -17,7 +37,7 @@ export function Summary() {
           />
 
         </header>
-        <strong>R$: 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -29,7 +49,7 @@ export function Summary() {
           />
 
         </header>
-        <strong>R$: 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -41,7 +61,7 @@ export function Summary() {
           />
 
         </header>
-        <strong>R$: 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
